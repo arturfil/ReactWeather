c7a20b9ec89020958c73d99e8031480c6f52be8c
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2017-06-01 01:01:00 -0400

    Style About sheet and add links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,18 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-    <h3>About</h3>
-    <p>Welcome to the about page</p>
+      <h1 className="small-centered text-center">About</h1>
+      <p className="text-center">Welcome to the about page</p>
+      <p> </p>
+      <p>Here are some of the tools I used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - this was the Javascript framework that I used
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map api to search for weather data by city name
+        </li>
+      </ul>
     </div>
   )
 };
